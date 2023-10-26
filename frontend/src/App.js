158f0718d1d32797b8f7793b58fe47aaf2a51cdb
Sams <shams.iqbal@g.bracu.ac.bd>
2023-10-26 17:19:38 +0600

    Completed React Router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './components/Register';
 import Login from './components/Login';
-
+import Navigate from './components/Navigate';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    // <Register/>
-    <Login/>
+    <BrowserRouter>
+    <Navigate/>
+    <Routes>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
